221bc6e88d4238c3073fea7c4ede7b063abee7da
Paulo Caçador Fernandes <pcnandes@yahoo.com.br>
2018-10-03 00:29:22 -0300

    pagina de login

---
+++ proj_cond/frontend/src/pages/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <q-page class="flex">
+    <div class="row fit">
+      <div class="col-8">
+        testeeee
+      </div>
+      <div class="col-4">
+        <div class="column">
+          <q-field>
+            <q-input placeholder="Usuário"/>
+          </q-field>
+          <q-field>
+            <q-input type="password" placeholder="Senha"/>
+          </q-field>
+          <br/>
+          <q-btn color="primary" @click="submit()">Login</q-btn>
+        </div>
+      </div>
+    </div>
+  </q-page>
+</template>
+<style>
+</style>
+
+<script>
+import { QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput } from 'quasar'
+export default {
+  name: 'PageIndex',
+  components: { QBtn, QToolbar, QIcon, QToolbarTitle, QField, QInput },
+  data () {
+    return {
+      lista: []
+    }
+  },
+  created () { },
+  methods: { }
+}
+</script>
+<style>
+  .pagina {
+    backgroun-color: red;
+  }
+</style>
--- proj_cond/frontend/src/router/routes.js
+++ proj_cond/frontend/src/router/routes.js
@@ -2,9 +2,9 @@
 const routes = [
   {
     path: '/',
-    component: () => import('layouts/MyLayout.vue'),
+    component: () => import('layouts/Welcome.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      { path: '', component: () => import('pages/Login.vue') }
     ]
   }
 ]
