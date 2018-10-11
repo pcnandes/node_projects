2fae3753f130cb1fe062007df5e10d4a5f4b4e93
“Paulo <pcnandes@yahoo.com.br>
2018-10-11 17:01:44 -0300

    criacao da pagina inicial

---
+++ proj_cond/frontend/src/pages/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-page class="flex flex-center">
+    Usuario logado!!!
+    teste{{lista}}
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+
+  data () {
+    return {
+      lista: []
+    }
+  },
+  created () {
+    this.listar()
+  },
+  methods: {
+    listar () {
+      this.$axios.get('/lista_sec')
+        .then((response) => {
+          console.log(response)
+          this.lista = response.data
+        })
+        .catch(() => {
+          this.$q.notify({
+            color: 'negative',
+            position: 'top',
+            message: 'Loading failed',
+            icon: 'report_problem'
+          })
+        })
+    }
+  }
+}
+</script>
--- proj_cond/frontend/src/router/routes.js
+++ proj_cond/frontend/src/router/routes.js
@@ -4,7 +4,8 @@ const routes = [
     path: '/',
     component: () => import('layouts/Welcome.vue'),
     children: [
-      { path: '', component: () => import('pages/Login.vue') }
+      { path: '', component: () => import('pages/Login.vue') },
+      { path: '/home', component: () => import('pages/Home.vue') }
     ]
   }
 ]
